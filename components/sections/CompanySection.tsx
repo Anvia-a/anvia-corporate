3ca348fcba67b77646235374d1837f1e5bc4741c
Anvia-a <tnkk.71.a@gmail.com>
2026-03-02 02:56:35 +0900

    Update CompanySection.tsx

--- components/sections/CompanySection.tsx
+++ components/sections/CompanySection.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+/* eslint-disable react/no-unescaped-entities */
 import { AnimatedSection, AnimatedStagger, StaggerItem } from "@/components/ui/AnimatedSection";
 
 const companyDetails = [
